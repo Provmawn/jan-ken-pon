386ad9d124f3ea3a294a4d2db85754b20eb8f19e
Anthony Chavez <ajserebii@gmail.com>
2019-05-20 17:43:11 -0700

    chat added

--- app.js
+++ app.js
@@ -12,24 +12,23 @@ server.listen(2000);
 console.log("Connected...");
 
 var PLAYERS = [];
-var LOBBIES = [];
-
-var createLobby = function(player_pair) {
-
-}
+var roomno = 0;
 
 var io = require('socket.io')(server, {});
 io.sockets.on('connection', function(socket){
     console.log("User connected...");
-    socket.id = Math.random();
     PLAYERS.push(socket.id);
-    if (PLAYERS.length % 2 == 0) {
-        createLobby({
-            player_1: PLAYERS.shift(),
-            player_2: PLAYERS.shift()
-        });
 
-    }
+    /*
+    //Increase roomno 2 clients are present in a room.
+   if(io.nsps['/'].adapter.rooms["room-"+roomno] && io.nsps['/'].adapter.rooms["room-"+roomno].length > 1) roomno++;
+   socket.join("room-"+roomno);
+
+   //Send this event to everyone in the room.
+   io.sockets.in("room-"+roomno).emit('connectToRoom', "You are in room no. "+roomno);
+
+   */
+
     for (var key in PLAYERS) {
         console.log(PLAYERS[key]);
     }
@@ -38,11 +37,18 @@ io.sockets.on('connection', function(socket){
         console.log('You got a message: ' + msg.txt);
     });
 
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+      });
+     
     socket.on('disconnect', function(socket) {
-        console.log("User disconnected");
-        console.log("SIZE OF PLAYERS: " + PLAYERS.length);
-
+        console.log("User Has left...");
         delete PLAYERS[socket.id];
         PLAYERS.splice(socket.id, 1); // remove player from array
+
+        console.log("Number of live Connections: " + PLAYERS.length);
+        //socket.leave("room-"+roomno);
+
     });
 });
